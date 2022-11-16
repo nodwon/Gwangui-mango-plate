7767963e6d7afa375ba0d78c58b509dcbdf3bb29
노다원 <nodwon5024@gmail.com>
2022-11-16 14:27:19 +0900

    12

--- src/main/java/com/smart/project/web/home/act/HomeAct.java
+++ src/main/java/com/smart/project/web/home/act/HomeAct.java
@@ -2,27 +2,19 @@ package com.smart.project.web.home.act;
 
 import com.smart.project.common.vo.InternCookie;
 import com.smart.project.component.CommonCodeComponent;
-import com.smart.project.component.data.CodeObject;
 import com.smart.project.proc.Test;
 import com.smart.project.security.StudyCookieService;
-import com.smart.project.web.home.vo.TestVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.net.URLEncoder;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @Controller
@@ -33,6 +25,7 @@ public class HomeAct {
 	CommonCodeComponent commonCodeComponent;
 
 	final private Test test;
+	///
 
 
 	@RequestMapping("/")
