fa5a3961bae2ac643bfb5a18fc9d3e26f20f3e68
노다원 <nodwon5024@gmail.com>
2022-11-16 15:04:55 +0900

    wishlist 같이 제작중

---
+++ src/main/java/com/smart/project/web/wishList/Service/WishListService.java
@@ -0,0 +1,9 @@
+package com.smart.project.web.wishList.Service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service  // 해당클래스를 루트 컨테이너에 빈 객체로 생성해주는 어노테이션
+@RequiredArgsConstructor
+public class WishListService {
+}
