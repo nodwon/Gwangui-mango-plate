833ec2eb236b3b8265c59404a7bf0c4451e7aad1
노다원 <nodwon5024@gmail.com>
2022-11-16 14:56:38 +0900

    네이버 API 음식점 연결중 web > login > naver >> dddd 에 login.html 있습니다

---
+++ src/main/java/com/smart/project/web/login/Naver/dto/SearchImageRes.java
@@ -0,0 +1,28 @@
+package com.smart.project.web.login.Naver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchImageRes { // 출력결과 필드
+    private String lastBuildDate; // 검색 결과를 생성
+    private int total; // 검색
+    private int start;
+    private int display;
+    private List<SearchImageItem> items;
+
+    public static class SearchImageItem{
+        private String title;
+        private String link;
+        private String thumbnail;
+        private String sizeheight;
+        private String sizewidth;
+
+
+    }
+}
---
+++ src/main/java/com/smart/project/web/login/Naver/dto/SearchLocalRes.java
@@ -0,0 +1,33 @@
+package com.smart.project.web.login.Naver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchLocalRes {
+    private String lastBuildDate;// 검색 결과를 생성
+    private int total;// 검색 결과 문서의 총 개수의 미
+    private int start; // 검색 결과  문서중 문서의 시작점을 의미
+    private int display; // 검색된 검사 결과의 개수
+    private List<SearchLocalItem> items;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SearchLocalItem {
+        private String title; // 검색 결과 업체. 기관명 나타냄
+        private String link;  // 검색 결과 업체 상세 정보가 제공되는 네이버페이지 하이퍼 텍스트 link 나타냄
+        private String description; // 검색 결과 업체, 기관명에 대한 설명을 제공한다.
+        private String category; // 검색결과
+        private String telephone; // 빈문자열 반환, 과거에 제공되던 항목이라 하위 혼환을 위해 존재 전화번호
+        private String address; // 검색 결과 업체 기관명의 주소를 제공한다.
+        private String roadAddress; // 검색 결과 기관명의 도로명 주소를 제공한다.
+        private int mapx; // 검색결과 업체, 기관명 위치 정보의 x좌표를 제공한다.
+        private int mapy; // 검색결과 업체 기관명 위치 정보의 y좌표를 제공한다.
+    }
+}
