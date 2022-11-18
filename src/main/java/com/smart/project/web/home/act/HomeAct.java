5706b632b1bae8e2b77a2a085e983c60bda01629
박동명 <pdm1990@naver.com>
2022-11-18 09:22:12 +0900

    닉네임/프로필 이미지까지 값가져오기, 이메일은 아직

--- src/main/java/com/smart/project/web/home/act/HomeAct.java
+++ src/main/java/com/smart/project/web/home/act/HomeAct.java
@@ -1,16 +1,21 @@
 package com.smart.project.web.home.act;
 
+import com.google.gson.Gson;
 import com.smart.project.common.vo.InternCookie;
 import com.smart.project.component.CommonCodeComponent;
 import com.smart.project.proc.Test;
 import com.smart.project.security.StudyCookieService;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.mapping.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -75,9 +80,67 @@ public class HomeAct {
 		return "dddd/join";
 	}
 
-	@RequestMapping("/data")
-	@ResponseBody
-	public String homeData(){
-		return "index";
-	}
+//	@RequestMapping("/data")
+//	@ResponseBody
+//	public String homeData(){
+//		return "index";
+//	}
+
+	@RequestMapping("/admin")
+	public String admin(){ return "index"; }
+	@RequestMapping("/mango")
+	public String main(){ return "mango"; }
+
+//	@RequiredArgsConstructor
+//	@Controller
+//	@RequestMapping("/kakaojoin")
+//	public class SocialController{
+//		private final Environment env;
+//		private final RestTemplate restTemplate;
+//		private final Gson gson;
+//		private final KakaoService ksksoService;
+//
+//		@Value("${spring.url.base}")
+//		private  String baseUrl;
+//
+//		@Value("${spring.social.kakao.client_id}")
+//		private String kakaoClientId;
+//
+//		@Value("${spring.social.kakao.redirect")
+//		private String kakaoRedirect;
+//
+//		/**
+//		 * 카카오 로그인 페이지
+//		 */
+//		@GetMapping
+//		public ModelAndView socialLogin(ModelAndView mav){
+//
+//			StringBuilder loginUrl = new StringBuilder()
+//					.append(env.getProperty("spring.social.kakao.url.login"))
+//					.append("?client_id=").append(kakaoClientId)
+//					.append("&response_type=code")
+//					.append("&redirect_uri=").append(baseUrl).append(kakaoRedirect);
+//			mav.addObject("loginUrl",loginUrl);
+//			mav.setViewName("social/login");
+//			return mav;
+//		}
+//		/**
+//		 * 카카오 인증 완료후 리다이렉트 화면
+//		 */
+//		@GetMapping(value = "/kakao")
+//		public ModelAndView redirectKakao(ModelAndView mav, @RequestParam String code){
+//			mav.addObject("authInfo",kakaoService.getKakaoTokenInfo(code));
+//			mav.setViewName("social/redirectKakao");
+//			return mav;
+//		}
+//
+//
+//	}
+@RequestMapping("/")
+public String home(@RequestParam(value = "code", required = false) String code) throws Exception{
+	System.out.println("#########" + code);
+	return "dddd/login";
+}
+
+
 }
---
+++ src/main/java/com/smart/project/web/home/act/KakaoController.java
@@ -0,0 +1,28 @@
+package com.smart.project.web.home.act;
+
+import com.smart.project.web.home.KakaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+
+@Controller
+public class KakaoController {
+    @Autowired
+    private KakaoService kakaoService;
+
+    @RequestMapping("/mango")
+    public String home(@RequestParam(value = "code", required = false) String code) throws Exception{
+        System.out.println("#########" + code);
+        String access_Token = kakaoService.getAccessToken(code);
+        HashMap<String, Object> userInfo = kakaoService.getUserInfo(access_Token);
+        System.out.println("###access_Token#### : " + access_Token);
+//        System.out.println("###userInfo#### : " + userInfo.get("email"));
+        System.out.println("###nickname#### : " + userInfo.get("nickname"));
+        System.out.println("###profile_image#### : " + userInfo.get("profile_image"));
+        return "mango";
+    }
+
+}
