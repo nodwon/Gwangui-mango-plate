69b21fc28e58c052b20337f7250e4232ed217834
노다원 <nodwon5024@gmail.com>
2022-11-28 09:06:21 +0900

    merge2

--- src/main/java/com/smart/project/proc/Test.java
+++ src/main/java/com/smart/project/proc/Test.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.util.List;//
 
 @Master
 @Component
