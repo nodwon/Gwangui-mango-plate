177c7b8661d0ebe442b5a2ceb56940ddc232208a
노다원 <nodwon5024@gmail.com>
2022-12-05 14:21:51 +0900

    merge

--- src/main/resources/static/resources/js/mango/detailPage.js
+++ src/main/resources/static/resources/js/mango/detailPage.js
@@ -6,128 +6,16 @@ $(()=>{
 })
 export class detailPage{
     constructor() {
-       /* window.onload = function setTemplate() {
+       window.onload = function setTemplate() {
             document.getElementById('allComments').innerHTML = localStorage.getItem('template');
         };
 
-        const commentContainer = document.getElementById('allComments');
-        document.getElementById('addComments').addEventListener('click', function (ev) {
-            addComment(ev);
-        });
 
-        function addComment(ev) {
-            let useremail = $("#user").text(); // 중복
-            let title = $("#title").text(); // 중복
-            let rating = document.querySelector('input[name ="rating"]:checked').value; // 중복
-            let commentText, wrapDiv; // 입력창과 div감싸기
-            const textBox = document.createElement('div'); //  입력 창 div 만들기
-            const likeButton = document.createElement('button'); //버튼 만들기
-            const updateButton = document.createElement('button') // 수정 버튼
-            const deleteButton = document.createElement('button'); // 삭제 버튼
-            const ratingshow = document.createElement("a") // star 만들기
-            const idli = document.createElement("a"); //id li 만들기
-            const titleshow = document.createElement("a"); // title li 만들기
-            const img = document.createElement("img"); // img 추가 버튼 만들기
-            const updateDate = document.createElement('span');
-            // updateDate.innerHTML = getTime();
-            updateDate.className = "inserttime";
-            updateButton.innerHTML = "수정";
-            updateButton.className = "updateComment";
-            likeButton.innerHTML = 'Like';
-            likeButton.className = 'likeComment';
-            deleteButton.innerHTML = 'Delete';
-            deleteButton.className = 'deleteComment';
-            idli.innerHTML = useremail;
-            idli.className ="showId";
-            ratingshow.innerHTML =rating;
-            ratingshow.className ="showRating";
-            titleshow.innerHTML =title;
-            titleshow.className ="showTitle";
-            img.innerHTML ="사진";
-            img.className ="showImg";
-
-            if (hasClass(ev.target.parentElement, 'container')) {
-                const wrapDiv = document.createElement('li');
-                wrapDiv.className = 'wrapper';
-                commentText = document.getElementById('comment').value;
-                //document.getElementById('comment').value = '';
-                textBox.innerHTML = commentText;
-                wrapDiv.append(textBox,idli,ratingshow,titleshow,updateButton, likeButton, deleteButton);
-                commentContainer.appendChild(wrapDiv);}
-             else {
-                wrapDiv = ev.target.parentElement;
-                commentText = ev.target.parentElement.firstElementChild.value;
-                textBox.innerHTML = commentText;
-                wrapDiv.innerHTML = '';
-                wrapDiv.append(textBox,idli,ratingshow,titleshow, updateButton, likeButton, deleteButton);
-            }
-            setOnLocalStorage();
-        }
-
-        function setOnLocalStorage() { // 모든 댓글
-            localStorage.setItem('template', document.getElementById('allComments').innerHTML);
-
-        }
-
-        function hasClass(elem, className) { //삭제
-            return elem.className.split(' ').indexOf(className) > -1;
-        }
-
-        document.getElementById('allComments').addEventListener('click', function (e) {
-            if(hasClass(e.target,'showTitle') != title){ /// 제목이랑 같을때 만 보여주고 나머지는 hidden
-                e.target.hidden;
-            }
-            if (hasClass(e.target, 'addReply')) {
-                addComment(e);
-            } else if (hasClass(e.target, 'likeComment')) {
-                const likeBtnValue = e.target.innerHTML;
-                e.target.innerHTML = likeBtnValue !== 'Like' ? Number.parseInt(likeBtnValue) + 1 : 1;
-                setOnLocalStorage();
-            } else if (hasClass(e.target, 'cancelReply')) {
-                e.target.parentElement.innerHTML = '';
-                setOnLocalStorage();
-            } else if (hasClass(e.target, 'deleteComment')) {
-                let reviewcontents = $("#comment").val();
-                let useremail = $("#user").text();
-                let title = $("#title").text();
-                let rating = document.querySelector('input[name ="rating"]:checked').value;
-                let object = {
-                    "email" : useremail,
-                    "placename" : title,
-                    "rating" : rating
-                }
-                axios.post({
-                    method : "post",
-                    url : "data/deleteReply",
-                    params : object
-                }).then((result)=>{
-                    console.log(object);
-                    console.log(result.data);
-                })
-
-                e.target.parentElement.remove();
-            }
-        });
-        function getTime(){
-            let datetime = new Date();
-            let year = datetime.getFullYear();
-            let month = datetime.getMonth();
-            let day = datetime.getDate();
-            let hour = datetime.getHours();
-            let minute = datetime.getMinutes();
-            let scecond = datetime.getSeconds();
-            // let fullupdatetime = document.write(year+"년"+(month+1)+"월"+day+"일"+hour+"시"+minute+"분"+scecond+"초");
-           return  year+"년"+(month+1)+"월"+day+"일"+hour+"시"+minute+"분"+scecond+"초";
-
-        }
-
-        });*/
 
         this.modalEvent();
         this.wishListEvent();
         this.favoriteStore();
         this.reviewEvent();
-        this.replyDeleteEvent();
         /*$("#Nav").append(this.head);*/
 
 
@@ -366,40 +254,151 @@ export class detailPage{
                 })
         })
     }
-
-    //리뷰 삭제 클릭시 이벤트
-    replyDeleteEvent(){
-        $('.deleteReply').on("click",(e)=>{
-            let useremail = $("#user").text();
-            let title = $("#title").text();
-            /*let rating = $('input[name ="rating"]:checked').val();*/
-            let object = {
-                "email" : useremail,
-                "title" : title
-            }
-            console.log(object);
-            if(useremail!=null){
-            axios({
-                method : 'post',
-                url : 'data/deleteReply',
-                params : object
-            })
-                $(e.currentTarget).parent('.wrapper').remove();
-            }
-        })
-
-    }
-
-    //////////////////////////////////
-    //작성하기 버튼 클릭시
     reviewEvent() {
-        $("#addComments").on("click", ()=> {
+        const commentContainer = document.getElementById('allComments');
+        document.getElementById('addComments').addEventListener('click', function (ev) {
+            addComment(ev);
+        });
+        function addComment(ev) {
+            let useremail = $("#user").text(); // 중복
+            let title = $("#title").text(); // 중복
+            let rating = document.querySelector('input[name ="rating"]:checked').val(); // 중복
+            let commentText, wrapDiv; // 입력창과 div감싸기
+            const textBox = document.createElement('div'); //  입력 창 div 만들기
+            const likeButton = document.createElement('button'); //버튼 만들기
+            const updateButton = document.createElement('button') // 수정 버튼
+            const deleteButton = document.createElement('button'); // 삭제 버튼
+            const ratingshow = document.createElement("a") // star 만들기
+            const idli = document.createElement("a"); //id li 만들기
+            const titleshow = document.createElement("a"); // title li 만들기
+            const img = document.createElement("img"); // img 추가 버튼 만들기
+            const updateDate = document.createElement('span');
+            // updateDate.innerHTML = getTime();
+            updateDate.className = "inserttime";
+            updateButton.innerHTML = "수정";
+            updateButton.className = "updateComment";
+            likeButton.innerHTML = 'Like';
+            likeButton.className = 'likeComment';
+            deleteButton.innerHTML = 'Delete';
+            deleteButton.className = 'deleteComment';
+            idli.innerHTML = useremail;
+            idli.className = "showId";
+            ratingshow.innerHTML = rating;
+            ratingshow.className = "showRating";
+            titleshow.innerHTML = title;
+            titleshow.className = "showTitle";
+            img.innerHTML = "사진";
+            img.className = "showImg";
+
+            if (hasClass(ev.target.parentElement, 'container')) {
+                const wrapDiv = document.createElement('li');
+                wrapDiv.className = 'wrapper';
+                commentText = document.getElementById('comment').value;
+                //document.getElementById('comment').value = '';
+                textBox.innerHTML = commentText;
+                wrapDiv.append(textBox, idli, ratingshow, titleshow, updateButton, likeButton, deleteButton);
+                commentContainer.appendChild(wrapDiv);
+            } else {
+                wrapDiv = ev.target.parentElement;
+                commentText = ev.target.parentElement.firstElementChild.value;
+                textBox.innerHTML = commentText;
+                wrapDiv.innerHTML = '';
+                wrapDiv.append(textBox, idli, ratingshow, titleshow, updateButton, likeButton, deleteButton);
+            }
+
+        }
+
+        function setOnLocalStorage() { // 모든 댓글
+            localStorage.setItem('template', document.getElementById('allComments').innerHTML);
+
+        }
+
+        function hasClass(elem, className) { //삭제
+            return elem.className.split(' ').indexOf(className) > -1;
+        }
+
+        document.getElementById('allComments').addEventListener('click', function (e) {
+            if (hasClass(e.target, 'showTitle') != title) { /// 제목이랑 같을때 만 보여주고 나머지는 hidden
+                e.target.hidden;
+            }
+            if (hasClass(e.target, 'addReply')) {
+                addComment(e);
+            } else if (hasClass(e.target, 'likeComment')) {
+                const likeBtnValue = e.target.innerHTML;
+                e.target.innerHTML = likeBtnValue !== 'Like' ? Number.parseInt(likeBtnValue) + 1 : 1;
+                setOnLocalStorage();
+            } else if (hasClass(e.target, 'cancelReply')) {
+                e.target.parentElement.innerHTML = '';
+                setOnLocalStorage();
+            } else if (hasClass(e.target, 'deleteComment')) {
+                let reviewcontents = $("#comment").val();
+                let useremail = $("#user").text();
+                let title = $("#title").text();
+                let rating = document.querySelector('input[name ="rating"]:checked').value;
+                let object = {
+                    "email": useremail,
+                    "placename": title,
+                    "rating": rating
+                }
+                axios.post({
+                    method: "post",
+                    url: "data/deleteReply",
+                    params: object
+                }).then((result) => {
+                    console.log(object);
+                    console.log(result.data);
+                })
+
+                e.target.parentElement.remove();
+            }
+        });
+
+        function getTime() {
+            let datetime = new Date();
+            let year = datetime.getFullYear();
+            let month = datetime.getMonth();
+            let day = datetime.getDate();
+            let hour = datetime.getHours();
+            let minute = datetime.getMinutes();
+            let scecond = datetime.getSeconds();
+            // let fullupdatetime = document.write(year+"년"+(month+1)+"월"+day+"일"+hour+"시"+minute+"분"+scecond+"초");
+            return year + "년" + (month + 1) + "월" + day + "일" + hour + "시" + minute + "분" + scecond + "초";
+
+        }
+
+        //리뷰 삭제 클릭시 이벤트
+        // replyDeleteEvent()
+        // {
+        //     $('.deleteReply').on("click", (e) => {
+        //         let useremail = $("#user").text();
+        //         let title = $("#title").text();
+        //         /*let rating = $('input[name ="rating"]:checked').val();*/
+        //         let object = {
+        //             "email": useremail,
+        //             "title": title
+        //         }
+        //         console.log(object);
+        //         if (useremail != null) {
+        //             axios({
+        //                 method: 'post',
+        //                 url: 'data/deleteReply',
+        //                 params: object
+        //             })
+        //             $(e.currentTarget).parent('.wrapper').remove();
+        //         }
+        //     })
+        //
+        // }
+
+        //////////////////////////////////
+        //작성하기 버튼 클릭시
+        $("#addComments").on("click", () => {
             let reviewcontents = $("#comment").val();
             let useremail = $("#user").text();
             let title = $("#title").text();
-            // let rating = document.querySelector('input[name ="rating"]:checked').value;
-            // let reviewdate = $("#allComments > li:nth-child(28) > span").text();
-            let rating = $('input[name ="rating"]:checked').val();
+            let rating = document.querySelector('input[name ="rating"]:checked').value;
+            let reviewdate = $("#allComments > li:nth-child(28) > span").text();
+            //let rating = $('input[name ="rating"]:checked').val();
 
             if (useremail === "") {
                 alert("로그인 후 이용해주세요");
@@ -407,7 +406,7 @@ export class detailPage{
             } else if (reviewcontents == null) {
                 alert("내용을 입력해주세요");
                 return;
-            }else if (rating == null) {
+            } else if (rating == null) {
                 alert("별점을 매겨주세요");
                 return;
             }
@@ -420,46 +419,36 @@ export class detailPage{
             };
 
             let start = "";
-            for(var i =1 ;i<=rating;i++)
-            {
-                    start +='<i class="fa-solid fa-star"></i>'
+            for (var i = 1; i <= rating; i++) {
+                start += '<i class="fa-solid fa-star"></i>'
             }
 
+
             // 리뷰 쓴 데이터 append 시키기
-            let html1 = [
-            '<li class="wrapper">',
-               '<p>'+reviewcontents+'</p>',
-               '<p class="showId">'+ useremail+ '</p>',
-               '<p class="showRating">'+rating+' </p>',
-               start,
-               '<br>',
-               '<button class="updateComment">수정</button>',
-               '<button class="likeComment">like</button>',
-               '<button class="deleteComment deleteReply">delete</button>',
+            // let html1 = [
+            //     '<li class="wrapper">',
+            //     '<p>' + reviewcontents + '</p>',
+            //     '<p class="showId">' + useremail + '</p>',
+            //     '<p class="showRating">' + rating + ' </p>',
+            //     start,
+            //     '<br>',
+            //     '<button class="updateComment">수정</button>',
+            //     '<button class="likeComment">like</button>',
+            //     '<button class="deleteComment deleteReply">delete</button>',
+            //
+            //
+            //     '  </li>'].join('');
+            $('#allComments').append(setOnLocalStorage());
 
-
-          '  </li>'].join('');
-            $('#allComments').append(html1);
-
-            this.replyDeleteEvent();
+        //    this.replyDeleteEvent();
             //리뷰데이터를 저장
             axios({
-                method : "post",
-                url : '/saveReview',
-                params : comment
+                method: "post",
+                url: '/saveReview',
+                params: comment
             })
 
-
-
-
-            // $('[type*="radio"]').change(function () { 별로 바꾸는것 //
-            //     var me = $(this);
-            //     log.html(me.attr('value'));
-            // https://codepen.io/lsirivong/pen/nRNLYL
         });
-
-
-
 
         //리뷰 수정 버튼 눌렀을 시 수정 버튼은 숨기고 수정 완료버튼 보여주기
         let mf = false;
@@ -470,20 +459,20 @@ export class detailPage{
                 return;
             }
             mf = true;
-            $(".inlinereview").attr("readonly", false);
+            $("").attr("readonly", false);
             $(this).hide();
             $(this).next().show();
             $.ajax({
-                type:"POST",
-                url:"/saveReview",
-                data:'json',
-                contentType:"application/json; charset=utf-8",
-                success:function(result){
-                    if(callback){
+                type: "POST",
+                url: "/saveReview",
+                data: 'json',
+                contentType: "application/json; charset=utf-8",
+                success: function (result) {
+                    if (callback) {
                         callback(result);
                     }
                 },
-                error:function(err){
+                error: function (err) {
                     alert("리뷰를 삭제하지 못했습니다. 다시 시도해 주세요.");
                 }
             })
@@ -508,23 +497,23 @@ export class detailPage{
                 }
             )
             $.ajax({
-                type:"PUT",
-                url:"/buy/"+review.reviewnum,
-                data:JSON.stringify(review),
-                contentType:"application/json; charset=utf-8",
-                success:function(result){
-                    if(callback){
+                type: "PUT",
+                url: "",
+                data: JSON.stringify(review),
+                contentType: "application/json; charset=utf-8",
+                success: function (result) {
+                    if (callback) {
                         callback(result);
                     }
                 },
-                error:function(err){
+                error: function (err) {
                     alert("리뷰 수정 실패. 다시 시도해주세요~");
                 }
             })
         })
+
+
     }
-
-
 }
 
 
